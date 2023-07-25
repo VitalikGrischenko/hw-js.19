722fa490840791b64e15b503d6dad38e08119cb4
VitalikGrischenko <110487381+VitalikGrischenko@users.noreply.github.com>
2023-07-25 12:00:18 +0300

    123124354safd

---
+++ js/1.js
@@ -0,0 +1,15 @@
+//Напишіть функцію, яка отримує масив рядків і повертає новий масив,
+// в якому кожен з рядків складається з першої та останньої букви відповідної строки вхідного масиву.
+
+const array2 = ['apple', 'banana', 'orange', 'banana', 'grape', 'apple', 'kiwi'];
+
+function getFirstLastLettersArrey (arr) {
+    return arr.map(words => {
+      const [firstLetter, lastLetter] = [words[0], words[words.length - 1]];
+      return firstLetter + lastLetter;
+    });
+  }
+  
+
+  const newArray = getFirstLastLettersArrey(array2);
+  console.log(newArray);
